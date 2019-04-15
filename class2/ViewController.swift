dd58a1a3420d5087286dd155bcedb72191c911fa
L20102-02 <wangbb.cjcu@gmail.com>
2019-04-15 10:02:05 +0800

    this is class 2 project @ 20190415 10 a.m.

---
+++ class2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  class2
+//
+//  Created by L20102-02 on 2019/4/15.
+//  Copyright © 2019年 L20102-02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+
+    }
+
+
+}
+
