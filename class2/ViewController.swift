b12f406e56378387bfc5092570c26013fc2cf10e
L20102-02 <wangbb.cjcu@gmail.com>
2019-04-15 10:22:30 +0800

    source control commit test

--- class2/ViewController.swift
+++ class2/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-
+        //this is a test
+        /*
+            hello, this is wangbb
+         */
+        
     }
 
 
